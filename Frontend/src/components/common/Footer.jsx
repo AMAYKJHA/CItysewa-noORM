158785d3911942b31cd95f3ef9032129d2d08807
LibraAkaja <adhikaribiraj908@gmail.com>
2026-01-25 13:05:39 +0545

    Frontend

---
+++ Frontend/src/components/common/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return(
+        <footer style={{border: '2px solid red', padding:'25px'}}>
+            <h2>CitySewa</h2>
+            <h5>All rights reserved</h5>
+            <strong>For support and query:</strong>
+            <a href="mailto:adhikaribiraj908@gmail.com" target="_blank">adhikaribiraj908@gmail.com</a>
+        </footer>
+    );
+};
+
+export default Footer;
