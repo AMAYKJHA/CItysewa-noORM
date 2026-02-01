d625919aee0028faf89d1977805a2a84a697da61
LibraAkaja <adhikaribiraj908@gmail.com>
2026-02-01 10:04:16 +0545

    Add proper authentication

--- Frontend/src/pages/auth/login/Login.jsx
+++ Frontend/src/pages/auth/login/Login.jsx
@@ -3,11 +3,13 @@ import './../../../Style/Login.css';
 import { useState } from 'react';
 import {useNavigate} from "react-router-dom";
 import { customerLogin, providerLogin } from '../../../api/client';
+import { useAuth } from '../../../hooks/useAuth';
 
 const Login = () => {
+    const { login } = useAuth();
     // const [def, setDefault] = useState(1);  // 1 is for customers
     // const [login, setLogin] = useState(customerLogin);
-    const [role, setRole] = useState("Customer");
+    const [role, setRole] = useState("customer");
     const [credentials, setCredentials] = useState({
         email: "",
         password: "",
@@ -20,40 +22,28 @@ const Login = () => {
             [e.target.name]: e.target.value,
         });
     };
-    const handleRoleChange = (r) => {
-        // setDefault(r);
-        // setLogin(r ? customerLogin : providerLogin);
-        setRole(r);
-    };
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError("");
         try{
-            const login = (role === "Customer" ? customerLogin : providerLogin);
-            const response = await login({
-                email: credentials.email,
-                password: credentials.password,
-            });
-            localStorage.setItem("token", response.data.access);
-            navigate(role === "Customer" ? "/customer" : "/provider");
+            const loginApi = (role === "customer" ? customerLogin : providerLogin);
+            const response = await loginApi(credentials);
+            login({...response.data, role});
+            navigate(role === "customer" ? "/customer" : "/provider");
         } catch (err){
-            setError("Invalid email or password");
-            setCredentials({
-                email: "",
-                password: ""
-            });
-            console.error(err);
+            setError(err.response?.data?.message || "Invalid email or password");
+            setCredentials({ email: "", password: "" });
         }
     };
     return(
         <>
             <section className='login-as'>
-                <div className="login-as-op" onClick={()=>handleRoleChange("Customer")} style={{background:`${role==="Customer" ? 'rgba(135, 206, 235, 0.75)': 'none'}`}}>Customer Login</div>
-                <div className="login-as-op" onClick={()=>handleRoleChange("Provider")} style={{background:`${role==="Provider" ? 'rgba(135, 206, 235, 0.75)': 'none'}`}}>Provider Login</div>
+                <div className="login-as-op" onClick={()=>setRole("customer")} style={{background:`${role==="customer" ? 'rgba(135, 206, 235, 0.75)': 'none'}`}}>Customer Login</div>
+                <div className="login-as-op" onClick={()=>setRole("provider")} style={{background:`${role==="provider" ? 'rgba(135, 206, 235, 0.75)': 'none'}`}}>Provider Login</div>
             </section>
             <form className='login-form' onSubmit={handleSubmit}>
             <fieldset>
-                <legend>{role==="Customer"? "Customer": "Provider"} Login</legend>
+                <legend>{role==="customer"? "Customer": "Provider"} Login</legend>
                 {error && <p className='error'>{error}</p>}
                 <span>
                     <label htmlFor="username-email">Username/Email</label>
