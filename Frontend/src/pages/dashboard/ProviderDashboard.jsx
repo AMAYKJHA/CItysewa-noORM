dea83bd494e479e453a076d67dab33644febd6b9
LibraAkaja <adhikaribiraj908@gmail.com>
2026-02-01 10:05:17 +0545

    Add useAuth to authenticate

--- Frontend/src/pages/dashboard/ProviderDashboard.jsx
+++ Frontend/src/pages/dashboard/ProviderDashboard.jsx
@@ -1,8 +1,16 @@
 import "../../Style/Dashboard.css";
+import { useAuth } from "../../hooks/useAuth";
+
 const ProviderDashboard = () => {
+    const {user} = useAuth();
+    if(!user) return <p>Loading...</p>;
     return(
-        <section className="provider-dashboard">
+        <section className="provider-dashboard" style={{color:'white'}}>
             <h2>Provider Dashboard</h2>
+            <h3>Welcome, {user.first_name} {user.last_name}</h3>
+            {!user.is_verified && (
+                <p style={{color:'orange'}}>Your account is not verified yet</p>
+            )}
         </section>
     );
 };
