bb4be08c6ce40ec5f848036691a6199d5830eed7
Amay Jha <83952941+AMAYKJHA@users.noreply.github.com>
2026-02-11 19:37:22 +0545

    fix: Fixed serialization issues in addresses module

--- Backend/src/addresses/serializers.py
+++ Backend/src/addresses/serializers.py
@@ -1,4 +1,12 @@
 from rest_framework import serializers
+from rest_framework.exceptions import NotFound
+
+from src.accounts.tables import (
+    User
+)
+from src.accounts.messages import (
+    USER_NOT_FOUND
+)
 
 from .tables import (
     District,
@@ -12,8 +20,10 @@ from .constants import (
 )
 
 from .messages import (
+    ADDRESS_NOT_FOUND,
+    ADDRESS_ALREADY_EXISTS,
     DISTRICT_NOT_FOUND,
-    ADDRESS_ALREADY_EXISTS
+    LOCATION_NOT_FOUND
 )
 
 
@@ -41,8 +51,9 @@ class DistrictSerializer(serializers.Serializer):
     def to_representation(self, instance):
         data = super().to_representation(instance)
         if not data.get("name"):
-            data["name"] = District().get(id=instance.get("id"))
+            data["name"] = District().get(id=instance.get("id")).name
         return data
+    
     
 class LocationCreateSerializer(serializers.Serializer):
     area = serializers.CharField(max_length=100)
@@ -69,8 +80,7 @@ class LocationCreateSerializer(serializers.Serializer):
     def create(self, validated_data):
         location = validated_data.pop("location", None)
         if not location:
-            location = Location().create(**validated_data)
-        
+            location = Location().create(**validated_data)        
         return location.__dict__
    
     
@@ -79,13 +89,13 @@ class LocationSerializer(serializers.Serializer):
     area = serializers.CharField(max_length=100)
     ward = serializers.IntegerField(min_value=MIN_WARD_VALUE, max_value=MAX_WARD_VALUE)
     city = serializers.CharField(max_length=50)
-    district_id = serializers.IntegerField()
+    district_id = serializers.IntegerField(write_only=True)
+    district = serializers.SerializerMethodField()
     
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        district_serializer = DistrictSerializer(id=instance.get("district_id"))
-        data["district"] = district_serializer.data.get("name")
-        return data
+    def get_district(self, obj):
+        district_id = obj.get("district_id")
+        district_serializer = DistrictSerializer({"id":district_id})
+        return district_serializer.data
 
 
 class AddressCreateSerializer(serializers.Serializer):
@@ -94,6 +104,12 @@ class AddressCreateSerializer(serializers.Serializer):
     landmarks = serializers.CharField(max_length=150)
     
     def validate(self, attrs):
+        user = User().get(id=attrs.get("user_id"))
+        if not user:
+            raise serializers.ValidationError({
+                "message": USER_NOT_FOUND
+            })
+            
         address = Address().get(user_id=attrs.get("user_id"), location_id=attrs.get("location_id"))
         if address:
             raise serializers.ValidationError({
@@ -109,14 +125,18 @@ class AddressCreateSerializer(serializers.Serializer):
 
    
 class AddressSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     user_id = serializers.IntegerField()
-    location_id = serializers.IntegerField()
+    location_id = serializers.IntegerField(write_only=True)
     landmarks = serializers.CharField(max_length=150)
+    location = serializers.SerializerMethodField()
     
-    def to_representation(self, instance):
-        data =  super().to_representation(instance)
-        location = Location().get(id=instance.get("location_id"))
+    def get_location(self, obj):
+        location_id = obj.get("location_id")
+        location = Location().get(id=location_id)
+        if not location:
+            raise NotFound(detail=LOCATION_NOT_FOUND)
         location_serializer = LocationSerializer(location.__dict__)
-        data["location"] = location_serializer.data
-        return data
+        return location_serializer.data 
+    
     
