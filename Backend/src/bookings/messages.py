95b484d6e48de88374a87f7ba45652f9638f841f
Amay Jha <83952941+AMAYKJHA@users.noreply.github.com>
2026-02-17 21:37:30 +0545

    fix: Service provider and customer can	 be the same user

--- Backend/src/bookings/messages.py
+++ Backend/src/bookings/messages.py
@@ -1,2 +1,4 @@
 PROVIDER_NOT_AVAILABLE="Provider not avaliable for this time. Please choose another time."
 BOOKING_NOT_FOUND="Booking not found."
+
+SAME_PROVIDER_AND_CUSTOMER="Service provider and customer can`t be the same person."
