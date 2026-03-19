c68f7ddf7f5705c315998ad4b96a7c9e5f821f05
LibraAkaja <adhikaribiraj908@gmail.com>
2026-03-19 22:07:37 +0545

    Minor change

--- Frontend/src/pages/profiles/CustomerProfile.jsx
+++ Frontend/src/pages/profiles/CustomerProfile.jsx
@@ -1,10 +1,44 @@
+import { useEffect, useState } from "react";
 import { useAuth } from "../../hooks/useAuth";
 import "../../Style/SectionPages.css";
+import { fetchCustomerById } from "../../api/client";
 
 const CustomerProfile = () => {
     const { user } = useAuth();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [userInfo, setUserInfo] = useState(null);
 
     if (!user) return <div className="profile-page"><div className="dashboard-loading">Loading…</div></div>;
+
+    useEffect(() => {
+        const loadUser = async () => {
+            if (!user) return;
+            try {
+                const response = await fetchCustomerById(user.id);
+                const info = response.data;
+                setUserInfo(info);
+            } catch (err) {
+                setError("Failed to fetch info")
+                console.error(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+        loadUser();
+    },[]);
+
+    if(loading) {
+        return (
+            <p style={{textAlign:'center', height:'200px', placeContent:'center'}}>Loading Info...</p>
+        )
+    }
+
+    if(error) {
+        return(
+            <p style={{textAlign:'center', height:'200px', placeContent:'center'}}>{error}</p>
+        );
+    }
 
     return (
         <div className="profile-page">
@@ -15,16 +49,24 @@ const CustomerProfile = () => {
             <div className="profile-card">
                 <h2>Account</h2>
                 <div className="profile-row">
+                    <span className="label">Customer Id</span>
+                    <span className="value">{userInfo.id || "—"}</span>
+                </div>
+                <div className="profile-row">
                     <span className="label">Name</span>
-                    <span className="value">{user.first_name} {user.last_name}</span>
+                    <span className="value">{userInfo.first_name} {userInfo.last_name}</span>
+                </div>
+                <div className="profile-row">
+                    <span className="label">Gender</span>
+                    <span className="value">{userInfo.gender}</span>
                 </div>
                 <div className="profile-row">
                     <span className="label">Email</span>
-                    <span className="value">{user.email}</span>
+                    <span className="value">{userInfo.email || "—"}</span>
                 </div>
                 <div className="profile-row">
                     <span className="label">Phone</span>
-                    <span className="value">{user.phone || "—"}</span>
+                    <span className="value">{userInfo.phone || "—"}</span>
                 </div>
                 <div className="profile-row">
                     <span className="label">Role</span>
