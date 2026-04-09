cd1a60c6b6f33d5d6d500f457da17af4b8884e06
Amay Jha <83952941+AMAYKJHA@users.noreply.github.com>
2026-04-09 13:17:28 +0545

    prod: Added new keep alive endpoint for preventing sleep

--- Backend/config/urls.py
+++ Backend/config/urls.py
@@ -3,7 +3,9 @@ URL configuration for config project.
 """
 
 from django.urls import path, include
+from django.core.cache import cache
 from rest_framework.renderers import JSONRenderer
+from django.http import JsonResponse
 from django.shortcuts import redirect
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -19,10 +21,22 @@ def trigger_error(request):
     """
     division_by_zero = 1/0
 
+def keep_alive(request):
+    try:
+        cache.get_or_set("cache_active", "ok")
+        cache_msg = "Cache is working fine."        
+    except Exception:
+        cache_msg = "Cache is not working."
+    return JsonResponse({
+        "message": "Server is running.",
+        "cache": cache_msg
+        })
+    
 urlpatterns = [
     path("", homepage, name="home-page"),
     path("api/v1/", include('src.api.urls')),
     path("api/v1/schema", SpectacularAPIView.as_view(renderer_classes=[JSONRenderer]), name="schema"),
     path("api/v1/docs", SpectacularSwaggerView.as_view(url_name='schema'), name="swagger-ui"),
+    path("keepalive", keep_alive, name="keep-alive"),
     path("sentry-debug", trigger_error, name="sentry-debug")
 ]
